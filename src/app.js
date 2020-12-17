3bf0143eaccd202ebbe215604b138ee6c70f8936
Bello S Babakolo <fleetbeekay@gmail.com>
2020-12-17 15:55:10 +0100

    Fix rendering error

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import model from './models/weatherappmodel';
 import nav from './views/navigation';
 import weatherCard from './views/weather_report';
 import spinner from './views/spinner';
+import notFound from './views/not_found';
 import './style.css';
 
 export default () => {
@@ -19,15 +20,22 @@ export default () => {
     searchLocationForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const weatherContainer = document.querySelector('div#weather-report-card');
-        weatherContainer.remove();
+        if (weatherContainer) weatherContainer.remove();
         root.insertAdjacentHTML('beforeend', spinner());
         let searchTerm = e.target.elements['search-location'].value;
         model.load(searchTerm)
           .then( result => {
             const parseResult = model.parseData(result);
-            const spinner = document.querySelector('div.loader');
-            spinner.remove();
+            let spinnerElement = document.querySelector('div.loader');
+            spinnerElement.remove();
             root.insertAdjacentHTML('beforeend', weatherCard(parseResult));
-          }).catch(e => console.log("Search Unavailable"));
+          })
+          .catch(e => {
+            let spinnerElement = document.querySelector('div.loader');
+            let notFoundElement = document.querySelector('div.not-found');
+            if (notFoundElement) notFoundElement.remove();
+            spinnerElement.remove();
+            root.insertAdjacentHTML('beforeend', notFound(searchTerm));
+          });
     });
 };
