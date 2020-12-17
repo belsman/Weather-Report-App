4c94eaa1b560842f6af66e17c5dd75e7d5fd595c
Bello S Babakolo <fleetbeekay@gmail.com>
2020-12-17 15:10:56 +0100

    Add 'loading' functionality to when a search is sent

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import model from './models/weatherappmodel';
 import nav from './views/navigation';
 import weatherCard from './views/weather_report';
+import spinner from './views/spinner';
 import './style.css';
 
 export default () => {
@@ -19,12 +20,14 @@ export default () => {
         e.preventDefault();
         const weatherContainer = document.querySelector('div#weather-report-card');
         weatherContainer.remove();
+        root.insertAdjacentHTML('beforeend', spinner());
         let searchTerm = e.target.elements['search-location'].value;
         model.load(searchTerm)
           .then( result => {
             const parseResult = model.parseData(result);
-            
+            const spinner = document.querySelector('div.loader');
+            spinner.remove();
             root.insertAdjacentHTML('beforeend', weatherCard(parseResult));
-          });
+          }).catch(e => console.log("Search Unavailable"));
     });
 };
